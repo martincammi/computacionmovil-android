c950f47fe1a2cb5e3b06c82b2c44915675c291ca
martincammi <martincammi@14fbf2f9-edbd-e8e1-1de9-13a21768d6fc>
2013-09-27 18:02:00 +0000

    Google Maps Markers.

--- trunk/Where2Eat/src/com/where2eat/activities/RestaurantDetailActivity.java
+++ trunk/Where2Eat/src/com/where2eat/activities/RestaurantDetailActivity.java
@@ -6,11 +6,16 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.where2eat.R;
 import com.where2eat.model.Restaurant;
 
@@ -18,8 +23,14 @@ import com.where2eat.model.Restaurant;
 public class RestaurantDetailActivity extends FragmentActivity {
 	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
 	static final LatLng KIEL = new LatLng(53.551, 9.993);
+	static final LatLng CIUDAD_UNIVERSITARIA = new LatLng(-34.541672, -58.442189);
 	private Restaurant restaurantSelected;
 	private GoogleMap map;
+	
+	/*@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+	}*/
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +40,52 @@ public class RestaurantDetailActivity extends FragmentActivity {
 		loadRestaurant();
 		updateRestaurantView();
 		
-		 map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		View viewLayout = (View) findViewById(R.id.fullscreen_content_controls5);
+		
+		SupportMapFragment mapFrag = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        map = mapFrag.getMap();
+		
+		//MapFragment aa = (MapFragment) viewLayout.findViewById(R.id.map);
+		
+//		SupportMapFragment myMapFragment = new SupportMapFragment(){
+//		    @Override
+//		    public void onActivityCreated(Bundle savedInstanceState){
+//		    	getFragmentManager().findFragmentById(R.id.map);
+//		    }
+//		};
+		
+		 //map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 		 
-//		 if (map!=null){
-//		      Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-//		          .title("Hamburg"));
-//		      Marker kiel = map.addMarker(new MarkerOptions()
-//		          .position(KIEL)
-//		          .title("Kiel")
-//		          .snippet("Kiel is cool")
-//		          .icon(BitmapDescriptorFactory
-//		              .fromResource(R.drawable.ic_launcher)));
-//		   }
+		 //getFragmentManager().findFragmentByTag(tag)
+		 
+		 if (map!=null){
+		      
+			 Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG).title("Hamburg"));
+		      
+		      Marker kiel = map.addMarker(new MarkerOptions()
+		          .position(KIEL)
+		          .title("Kiel")
+		          .snippet("Kiel is cool")
+		          .icon(BitmapDescriptorFactory
+		              .fromResource(R.drawable.ic_launcher)));
+		      
+		      Marker ciudad = map.addMarker(new MarkerOptions()
+	          .position(CIUDAD_UNIVERSITARIA)
+	          .title("Ciudad universitaria")
+	          .snippet("Exactas is cool")
+	          .icon(BitmapDescriptorFactory
+	              .fromResource(R.drawable.ic_launcher)));
+		      
+		      
+		   }
+		 
+		 //map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+		 
+		 map.moveCamera(CameraUpdateFactory.newLatLngZoom(CIUDAD_UNIVERSITARIA, 7));
+		 
+		 map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null); 
 		
 	}
 
