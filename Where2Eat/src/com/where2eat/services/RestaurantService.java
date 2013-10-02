acbe5b17d69c884e5cafea3000566a5c5acd7dd2
martincammi <martincammi@14fbf2f9-edbd-e8e1-1de9-13a21768d6fc>
2013-10-02 22:20:06 +0000

    Reagrupación de Restaurants

--- Where2Eat/src/com/where2eat/services/RestaurantService.java
+++ Where2Eat/src/com/where2eat/services/RestaurantService.java
@@ -9,27 +9,57 @@ import java.util.List;
 import android.location.Location;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.where2eat.model.FoodType;
 import com.where2eat.model.Restaurant;
+
 
 public class RestaurantService {
 
+	private List<Restaurant> restaurants;
 	
-	public List<Restaurant> getRestaurantsByName(String name, Location currentLocation){
-		
-		List<Restaurant> restaurants = new ArrayList<Restaurant>();
-		restaurants.add(new Restaurant(name, "Cabildo 2900", "4794-3452", -34.555240, -58.462556));
-		restaurants.add(new Restaurant("Romario", "Libertador 2900", "4833-1422", -34.576603, -58.410950));
-		restaurants.add(new Restaurant("TGI Friday", "Alvarez Thomas 1900", "4345-8482", -34.578643, -58.468296));
-		
-		return restaurants;
+	public RestaurantService(){
+		initializeRestaurants();
 	}
 	
-	public List<Restaurant> getRestaurantsBySpeciality(String culinarySpecialty){
+	private void initializeRestaurants() {
 		
-		List<Restaurant> restaurants = new ArrayList<Restaurant>();
-		restaurants.add(new Restaurant("Miranda", "Costa Rica 5602, Palermo", "4794-3452", -34.582530, -58.434597));
-		restaurants.add(new Restaurant("Rolaso", "Julián Alvarez 600, Villa Crespo", "4794-3452", -34.599205, -58.434049));
-		restaurants.add(new Restaurant("Parrilla Marucha", "11 de Septiembre 3702, Nuñez", "4345-8482", -34.544450, -58.462095));
+		restaurants.add(new Restaurant("Family", "Cabildo 2900", "4794-3452", -34.555240, -58.462556, FoodType.PARRILLA));
+		restaurants.add(new Restaurant("Romario", "Libertador 2900", "4833-1422", -34.576603, -58.410950, FoodType.PIZZERIA));
+		restaurants.add(new Restaurant("TGI Friday", "Alvarez Thomas 1900", "4345-8482", -34.578643, -58.468296, FoodType.AMERICANA));
+		restaurants.add(new Restaurant("Miranda", "Costa Rica 5602, Palermo", "4794-3452", -34.582530, -58.434597, FoodType.PARRILLA));
+		restaurants.add(new Restaurant("Rolaso", "Julián Alvarez 600, Villa Crespo", "4794-3452", -34.599205, -58.434049, FoodType.ITALIANA));
+		restaurants.add(new Restaurant("Parrilla Marucha", "11 de Septiembre 3702, Nuñez", "4345-8482", -34.544450, -58.462095, FoodType.PARRILLA));
+	}
+
+	public List<Restaurant> getRestaurantsByName(String searchField, Location currentLocation){
+		
+		List<Restaurant> result = new ArrayList<Restaurant>();
+		
+		for (Restaurant restaurant : restaurants) {
+			
+			String restaurantName = restaurant.getName().toLowerCase();
+		
+			if(restaurantName.contains(searchField.toLowerCase())){
+				result.add(restaurant);
+			}
+		}
+		
+		return result;
+	}
+	
+	public List<Restaurant> getRestaurantsBySpeciality(String searchField){
+		
+		List<Restaurant> result = new ArrayList<Restaurant>();
+		searchField = searchField.replace(" ", ""); 
+		
+		for (Restaurant restaurant : restaurants) {
+			
+			String restaurantFoodType = restaurant.getFoodType().name().toLowerCase();
+		
+			if(restaurantFoodType.contains(searchField.toLowerCase())){
+				result.add(restaurant);
+			}
+		}
 		
 		return restaurants;
 	}
